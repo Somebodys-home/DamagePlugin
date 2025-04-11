5a01b706bd4e2ed4756452fe6c8e23d116b8304a
Gabriel <gabrieln38@nycstudents.net>
2025-04-11 18:51:00 -0400

    almost finished the custom damage system, just working on the item descriptions
    Signed-off-by: Gabriel <gabrieln38@nycstudents.net>

---
+++ src/main/java/io/github/Gabriel/damagePlugin/DamagePlugin.java
@@ -0,0 +1,40 @@
+package io.github.Gabriel.damagePlugin;
+
+import io.github.Gabriel.damagePlugin.customDamage.*;
+import io.github.Gabriel.damagePlugin.customDamage.commands.CheckDamageCommand;
+import io.github.Gabriel.damagePlugin.customDamage.commands.SetDamageCommand;
+import io.github.Gabriel.damagePlugin.customDamage.damageLore.DamageLoreListener;
+import io.github.Gabriel.damagePlugin.noDamage.NoDamageListener;
+import io.github.Gabriel.damagePlugin.noDamage.NoDamageManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DamagePlugin extends JavaPlugin {
+    private NoDamageManager noDamageManager;
+    private DamageKeys damageKeys;
+    private CustomDamager customDamager;
+
+    @Override
+    public void onEnable() {
+        noDamageManager = new NoDamageManager(this);
+        damageKeys = new DamageKeys(this);
+        customDamager = new CustomDamager(this);
+
+        getCommand("setDamage").setExecutor(new SetDamageCommand(this));
+        getCommand("checkdamage").setExecutor(new CheckDamageCommand(this));
+        getServer().getPluginManager().registerEvents(new NoDamageListener(this), this);
+        getServer().getPluginManager().registerEvents(new DamageListener(this), this);
+        getServer().getPluginManager().registerEvents(new DamageLoreListener(this), this);
+    }
+
+    public NoDamageManager getDamageAttributesManager() {
+        return noDamageManager;
+    }
+
+    public DamageKeys getDamageKeys() {
+        return damageKeys;
+    }
+
+    public CustomDamager getCustomDamager() {
+        return customDamager;
+    }
+}
