8883586c8fe3a556e5efe1b8433b19da4cc99ac6
NoOne_NoBody <hypixelnerd347@gmail.com>
2025-07-27 00:31:27 -0400

    imported defensesystem and made CustomDamager work with that now

--- src/main/java/io/github/Gabriel/damagePlugin/DamagePlugin.java
+++ src/main/java/io/github/Gabriel/damagePlugin/DamagePlugin.java
@@ -5,6 +5,10 @@ import io.github.Gabriel.damagePlugin.customDamage.CustomDamager;
 import io.github.Gabriel.damagePlugin.customDamage.DamageLore;
 import io.github.Gabriel.damagePlugin.noDamage.NoDamageListener;
 import io.github.Gabriel.damagePlugin.noDamage.NoDamageManager;
+import io.github.NoOne.nMLPlayerStats.NMLPlayerStats;
+import io.github.NoOne.nMLPlayerStats.profileSystem.ProfileManager;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class DamagePlugin extends JavaPlugin {
@@ -12,12 +16,25 @@ public final class DamagePlugin extends JavaPlugin {
     private NoDamageManager noDamageManager;
     private CustomDamager customDamager;
     private DamageLore damageLore;
+    private static NMLPlayerStats nmlPlayerStats;
+    private ProfileManager profileManager;
 
     @Override
     public void onEnable() {
         damagePlugin = this;
         noDamageManager = new NoDamageManager(this);
         customDamager = new CustomDamager(this);
+        damageLore = new DamageLore();
+
+        Plugin plugin = Bukkit.getPluginManager().getPlugin("NMLPlayerStats");
+        if (plugin instanceof NMLPlayerStats statsPlugin) {
+            nmlPlayerStats = statsPlugin;
+            profileManager = nmlPlayerStats.getProfileManager();
+        } else {
+            getLogger().severe("Failed to find NMLPlayerStats! Disabling...");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
 
         getServer().getPluginManager().registerEvents(new NoDamageListener(this), this);
         getServer().getPluginManager().registerEvents(new DamageListener(this), this);
@@ -35,5 +52,15 @@ public final class DamagePlugin extends JavaPlugin {
         return customDamager;
     }
 
-    public DamageLore getDamageLoreUtil() {return damageLore;}
+    public DamageLore getDamageLore() {
+        return damageLore;
+    }
+
+    public static NMLPlayerStats getNmlPlayerStats() {
+        return nmlPlayerStats;
+    }
+
+    public ProfileManager getProfileManager() {
+        return profileManager;
+    }
 }
--- src/main/java/io/github/Gabriel/damagePlugin/customDamage/CustomDamager.java
+++ src/main/java/io/github/Gabriel/damagePlugin/customDamage/CustomDamager.java
@@ -1,10 +1,15 @@
 package io.github.Gabriel.damagePlugin.customDamage;
 
 import io.github.Gabriel.damagePlugin.DamagePlugin;
+import io.github.NoOne.nMLDefenses.DefenseType;
+import io.github.NoOne.nMLPlayerStats.profileSystem.ProfileManager;
+import io.github.NoOne.nMLPlayerStats.statSystem.Stats;
 import org.bukkit.damage.DamageSource;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.metadata.FixedMetadataValue;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -21,7 +26,58 @@ public class CustomDamager {
     // todo: get rid of damage messages eventually
     // for multiple damage types
     public static void doDamage(LivingEntity target, LivingEntity damager, Map<DamageType, Double> damageSplits) {
+        Stats targetStats = new ProfileManager(plugin.getNmlPlayerStats()).getPlayerProfile(target.getUniqueId()).getStats();
+        HashMap<DefenseType, Integer> resistedTypes = new HashMap<>();
+
+        if (targetStats != null) { // if damaging a player / has playerstats
+            if (targetStats.getDefense() != 0) {
+                resistedTypes.put(DefenseType.DEFENSE, targetStats.getDefense());
+            }
+            if (targetStats.getPhysicalResist() != 0) {
+                resistedTypes.put(DefenseType.PHYSICALRESIST, targetStats.getPhysicalResist());
+            }
+            if (targetStats.getFireResist() != 0) {
+                resistedTypes.put(DefenseType.FIRERESIST, targetStats.getFireResist());
+            }
+            if (targetStats.getColdResist() != 0) {
+                resistedTypes.put(DefenseType.COLDRESIST, targetStats.getColdResist());
+            }
+            if (targetStats.getEarthResist() != 0) {
+                resistedTypes.put(DefenseType.EARTHRESIST, targetStats.getEarthResist());
+            }
+            if (targetStats.getLightningResist() != 0) {
+                resistedTypes.put(DefenseType.LIGHTNINGRESIST, targetStats.getLightningResist());
+            }
+            if (targetStats.getAirResist() != 0) {
+                resistedTypes.put(DefenseType.AIRRESIST, targetStats.getAirResist());
+            }
+            if (targetStats.getLightResist() != 0) {
+                resistedTypes.put(DefenseType.LIGHTRESIST, targetStats.getLightResist());
+            }
+            if (targetStats.getDarkResist() != 0) {
+                resistedTypes.put(DefenseType.DARKRESIST, targetStats.getDarkResist());
+            }
+        }
+
         double totalDamage = 0;
+        if (!resistedTypes.isEmpty()) { // has A resistance
+            for (Map.Entry<DamageType, Double> entry : damageSplits.entrySet()) {
+                if (entry.getKey() != DamageType.PURE) {
+                    entry.setValue(entry.getValue() - resistedTypes.get(DefenseType.DEFENSE));
+                }
+
+                switch (entry.getKey()) {
+                    case PHYSICAL -> entry.setValue(entry.getValue() - resistedTypes.get(DefenseType.PHYSICALRESIST));
+                    case FIRE -> entry.setValue(entry.getValue() - resistedTypes.get(DefenseType.FIRERESIST));
+                    case COLD -> entry.setValue(entry.getValue() - resistedTypes.get(DefenseType.COLDRESIST));
+                    case EARTH -> entry.setValue(entry.getValue() - resistedTypes.get(DefenseType.EARTHRESIST));
+                    case LIGHTNING -> entry.setValue(entry.getValue() - resistedTypes.get(DefenseType.LIGHTNINGRESIST));
+                    case AIR -> entry.setValue(entry.getValue() - resistedTypes.get(DefenseType.AIRRESIST));
+                    case LIGHT -> entry.setValue(entry.getValue() - resistedTypes.get(DefenseType.LIGHTRESIST));
+                    case DARK -> entry.setValue(entry.getValue() - resistedTypes.get(DefenseType.DARKRESIST));
+                }
+            }
+        }
 
         for (Map.Entry<DamageType, Double> entry : damageSplits.entrySet()) {
             damageInstance.put(target.getUniqueId(), new DamageInstance(entry.getKey(), entry.getValue()));
@@ -38,6 +94,56 @@ public class CustomDamager {
 
     // for single type damage
     public static void doDamage(LivingEntity target, LivingEntity damager, double damage, DamageType damageType) {
+        Stats targetStats = new ProfileManager(DamagePlugin.getNmlPlayerStats()).getPlayerProfile(target.getUniqueId()).getStats();
+        HashMap<DefenseType, Integer> resistedTypes = new HashMap<>();
+
+        if (targetStats != null) { // if damaging a player / has playerstats
+            if (targetStats.getDefense() != 0) {
+                resistedTypes.put(DefenseType.DEFENSE, targetStats.getDefense());
+            }
+            if (targetStats.getPhysicalResist() != 0) {
+                resistedTypes.put(DefenseType.PHYSICALRESIST, targetStats.getPhysicalResist());
+            }
+            if (targetStats.getFireResist() != 0) {
+                resistedTypes.put(DefenseType.FIRERESIST, targetStats.getFireResist());
+            }
+            if (targetStats.getColdResist() != 0) {
+                resistedTypes.put(DefenseType.COLDRESIST, targetStats.getColdResist());
+            }
+            if (targetStats.getEarthResist() != 0) {
+                resistedTypes.put(DefenseType.EARTHRESIST, targetStats.getEarthResist());
+            }
+            if (targetStats.getLightningResist() != 0) {
+                resistedTypes.put(DefenseType.LIGHTNINGRESIST, targetStats.getLightningResist());
+            }
+            if (targetStats.getAirResist() != 0) {
+                resistedTypes.put(DefenseType.AIRRESIST, targetStats.getAirResist());
+            }
+            if (targetStats.getLightResist() != 0) {
+                resistedTypes.put(DefenseType.LIGHTRESIST, targetStats.getLightResist());
+            }
+            if (targetStats.getDarkResist() != 0) {
+                resistedTypes.put(DefenseType.DARKRESIST, targetStats.getDarkResist());
+            }
+        }
+
+        if (!resistedTypes.isEmpty()) { // has A resistance
+            if (damageType != DamageType.PURE) {
+                damage -= resistedTypes.get(DefenseType.DEFENSE);
+            }
+
+            switch (damageType) {
+                case PHYSICAL -> damage -= resistedTypes.get(DefenseType.DEFENSE);
+                case FIRE -> damage -= resistedTypes.get(DefenseType.DEFENSE);
+                case COLD -> damage -= resistedTypes.get(DefenseType.DEFENSE);
+                case EARTH -> damage -= resistedTypes.get(DefenseType.DEFENSE);
+                case LIGHTNING -> damage -= resistedTypes.get(DefenseType.DEFENSE);
+                case AIR -> damage -= resistedTypes.get(DefenseType.DEFENSE);
+                case LIGHT -> damage -= resistedTypes.get(DefenseType.DEFENSE);
+                case DARK -> damage -= resistedTypes.get(DefenseType.DEFENSE);
+            }
+        }
+
         damageInstance.put(target.getUniqueId(), new DamageInstance(damageType, damage));
 
         if (damager instanceof Player player) {
@@ -45,7 +151,6 @@ public class CustomDamager {
         }
 
         target.setMetadata("recursive_block", new FixedMetadataValue(plugin, true));
-        damager.sendMessage("damage: " + damage); // test msg
         target.damage(damage, damager);
     }
 }
