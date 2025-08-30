cd6eca73ffb5afd0374a3a7a45ed4591559fb88b
NoOne_NoBody <hypixelnerd347@gmail.com>
2025-08-29 22:18:32 -0400

    ok now i fully fixed it

--- src/main/java/io/github/Gabriel/damagePlugin/customDamage/DamageListener.java
+++ src/main/java/io/github/Gabriel/damagePlugin/customDamage/DamageListener.java
@@ -7,6 +7,7 @@ import org.bukkit.Material;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.inventory.ItemStack;
@@ -21,10 +22,9 @@ public class DamageListener implements Listener {
         this.plugin = plugin;
     }
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.MONITOR)
     public void doCustomDamage(CustomDamageEvent event) {
         if (!event.isCancelled()) {
-            System.out.print("cancelled? " + event.isCancelled());
             CustomDamager.doDamage(event.getTarget(), event.getDamager(), event.getDamageSplits());
         }
     }
